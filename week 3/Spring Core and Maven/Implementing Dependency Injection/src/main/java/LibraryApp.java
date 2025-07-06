78347a0e2d247f9c198de7a7c3a787920a69eb85
Pravalika <pravalikayadavm@gmail.com>
2025-07-06 20:53:03 +0530

    added

---
+++ week 3/Spring Core and Maven/Implementing Dependency Injection/src/main/java/LibraryApp.java
@@ -0,0 +1,13 @@
+import com.library.service.BookService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class LibraryApp {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        BookService bookService = (BookService) context.getBean("bookService");
+
+        bookService.addBook("Pride and Prejudice");
+    }
+}
